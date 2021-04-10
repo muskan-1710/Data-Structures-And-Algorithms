b2e173aea823bfa8b7c9fb5c6414280bf5c004a9
muskan-1710 <75213806+muskan-1710@users.noreply.github.com>
2021-04-10 23:04:55 +0530

    Update SubsequencesOfString.cpp

--- SubsequencesOfString.cpp
+++ SubsequencesOfString.cpp
@@ -10,7 +10,7 @@ void printSubsequence(string input, string output)
         return;
     }
  
-   str(1), output + input[0]);
+  printSubsequence(input.substr(1), output + input[0]);
  
     
     printSubsequence(input.substr(1), output);
